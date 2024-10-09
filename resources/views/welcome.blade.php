de33f95126b8d84492f5e0b30d8f52be83478586
Nicholasshizuka <110775343+Nicholasshizuka@users.noreply.github.com>
2024-10-09 10:28:15 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,10 +50,6 @@
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="/peminjaman" class="underline text-gray-900 dark:text-white">Peminjaman</a></div>
                             </div>
                         </div>
-
-                        
-
-                    
                 </div>
             </div>
         </div>
